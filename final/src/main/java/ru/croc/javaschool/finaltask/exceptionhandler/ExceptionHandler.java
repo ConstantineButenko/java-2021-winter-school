0a4ea53afb8c44b2ce0c76c82cf529de5a80c7be
ConstantineButenko <76672330+ConstantineButenko@users.noreply.github.com>
2021-04-29 17:34:26 +0300

    Update ExceptionHandler.java

--- final/src/main/java/ru/croc/javaschool/finaltask/exceptionhandler/ExceptionHandler.java
+++ final/src/main/java/ru/croc/javaschool/finaltask/exceptionhandler/ExceptionHandler.java
@@ -4,7 +4,16 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
+/** Класс-обработчик возникающих исключений
+ * 
+ */
+
 public class ExceptionHandler {
+    /** Метод обработки исключения. Записывает в текстовый файл сообщение об исключении и дату-время его возникновения.
+     * 
+     * @param e передаваемое исключение
+     * @param fileName файл, в который будет осуществляться запись лога
+     */
         public void handleException(Exception e, String fileName) {
 
             try (FileWriter writer = new FileWriter(fileName, true)) {
